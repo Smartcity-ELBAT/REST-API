ec79e58c9db522af0c2e5f5b0a69a61bd26bc3f9
Christophe Bernard <DrakeXorn@users.noreply.github.com>
2020-11-26 16:25:27 +0100

    Added some lines to the database creation file

--- scripts/SQL/createDB.sql
+++ scripts/SQL/createDB.sql
@@ -92,13 +92,65 @@ create table user_access_level (
     primary key (user_id, access_level)
 );
 
-insert into locality(city, postal_code) values ('fruitCity', '5100');
-insert into locality(city, postal_code) values ('PommeTown', '5300');
+insert into locality(city, postal_code) values ('Naninne', '5100');
+insert into locality(city, postal_code) values ('Wépion', '5100');
 
 insert into address (street, number, country, locality_city, postal_code)
-values ('rue des pommes', 5, 'Belgique', 'fruitCity', '5100');
+values ('rue des pommes', 5, 'Belgique', 'Naninne', '5100');
 
 insert into address (street, number, country, locality_city, postal_code)
-VALUES ('rue des cerises', 10, 'Belgique', 'PommeTown', '5300');
+VALUES ('rue des cerises', 10, 'Belgique', 'Wépion', '5100');
 
-insert into access_level (access_level) VALUES ('customer');
+insert into address (street, number, country, locality_city, postal_code)
+VALUES ('rue des bananes', 15, 'Belgique', 'Wépion', '5100');
+
+insert into address (street, number, country, locality_city, postal_code)
+VALUES ('rue des poires', 20, 'Belgique', 'Wépion', '5100');
+
+insert into person (username, password, last_name, first_name, birth_date, gender, phone_number, email, is_positive_to_covid_19, address_id)
+VALUES ('pomme123', '$2b$10$GTjeVpITeh6K.Oh8oGq5NOn/Ywvd5Hnruc6HUCyfeCQF2hP6EdIW.', 'Fruit', 'Pomme', '2015-05-10', 'f', '1234567890', 'pomme@fruit.be', null, '1' );
+
+insert into person (username, password, last_name, first_name, birth_date, gender, phone_number, email, is_positive_to_covid_19, address_id)
+VALUES ('poire123', '$2b$10$5fixCge7j6I2BLSGlQ7eV.YGrKLaK.nsdglpZ4Hg3P2etvqUUCAvK', 'Fruit', 'Poire', '2013-08-18', 'm', '0147852369', 'poire@fruit.be', null, '4' );
+
+insert into person (username, password, last_name, first_name, birth_date, gender, phone_number, email, is_positive_to_covid_19, address_id)
+VALUES ('kiwi123', '$2b$10$7APucUJHm93.lIZYGuv.EegpIXWRu9VXKH4PfEwWzOXEScQCbiDfy', 'Fruit', 'Kiwi', '2010-05-20', 'm', '0147852396', 'kiwi@fruit.be', null, '2' );
+insert into establishment (name, phone_number, vat_number, email, category, address_id)
+VALUES ('Sams Lunch', '9876543210', 'BE9874563210', 'samslunch@restau.be', 'sandwicherie', 1);
+
+insert into establishment (name, phone_number, vat_number, email, category, address_id)
+VALUES ('Yayami', '0123456789', 'BE3692580147', 'yayami@restau.be', 'asiatique', 3);
+
+insert into "table" (establishment_id, seats_nbr, is_outside)
+VALUES (1, 4, true);
+
+insert into "table" (establishment_id, seats_nbr, is_outside)
+VALUES (1, 6, false);
+
+insert into "table" (establishment_id, seats_nbr, is_outside)
+VALUES (2, 4, true);
+
+insert into "table" (establishment_id, seats_nbr, is_outside)
+VALUES (2, 6, false);
+
+insert into "table" (establishment_id, seats_nbr, is_outside)
+VALUES (2, 6, false);
+
+insert into access_level(access_level) values ( 'customer' );
+insert into access_level(access_level) values ( 'admin' );
+insert into access_level(access_level, establishment_id) values ( 'waiter_E1',  1);
+
+insert into reservation (person_id, date_time_reserved, arriving_time, exit_time, customers_nbr, additional_info, is_cancelled, table_id, establishment_id)
+VALUES (1, '2020-10-12 20:30', null, null, 4, null, null, 1, 1);
+
+insert into reservation (person_id, date_time_reserved, arriving_time, exit_time, customers_nbr, additional_info, is_cancelled, table_id, establishment_id)
+VALUES (2, '2020-11-15 19:30', null, null, 6, null, null, 2, 2);
+
+insert into reservation (person_id, date_time_reserved, arriving_time, exit_time, customers_nbr, additional_info, is_cancelled, table_id, establishment_id)
+VALUES (2, '2020-05-26 21:30', null, null, 2, null, null, 3, 2);
+
+insert into user_access_level (user_id, access_level) VALUES ( 1, 'admin' );
+insert into user_access_level (user_id, access_level) VALUES ( 1, 'customer' );
+insert into user_access_level (user_id, access_level) VALUES ( 2, 'customer' );
+insert into user_access_level (user_id, access_level) VALUES ( 3, 'customer' );
+insert into user_access_level (user_id, access_level) VALUES ( 3, 'waiter_E1' );
