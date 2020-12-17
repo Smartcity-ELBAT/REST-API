b92e7311d34e55853f2c98a04539ab2fc95a2e8d
Christophe Bernard <DrakeXorn@users.noreply.github.com>
2020-12-17 23:02:24 +0100

    Removed CORS handling from app.js

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 require("dotenv").config();
 const Router = require("./route");
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.json());
 app.use(Router);
 
